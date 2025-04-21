a68cbebd5fa7c894fc4a2b312f3d829efd6d4b88
Scrimali Anthony <anthony.scrimali@gmail.com>
2025-04-21 17:12:03 +0200

    [feat] : Ajout d'une classe GameEngine pour vider le main

---
+++ src/battleship/app/GameEngine.java
@@ -0,0 +1,62 @@
+package battleship.app;
+
+import battleship.exceptions.*;
+import battleship.model.Player;
+import battleship.ui.Display;
+
+import java.util.Scanner;
+
+class GameEngine {
+    Scanner sc = new Scanner(System.in);
+    Player player1;
+
+    void startGame() {
+        placeShip();
+        fire();
+        sc.close();
+    }
+
+    private void placeShip() {
+        player1 = new Player();
+        System.out.println(player1.getBoardAsString());
+
+        for(int i = 0; i < 5; i++) {
+            boolean valid = false;
+            while(!valid) {
+                System.out.println(Display.placeShip(i));
+                String shipPositions = sc.nextLine();
+                try {
+                    player1.createFleet(shipPositions, i);
+                    System.out.println(player1.getLengthOfShipAsString(player1.getShip(i)));
+                    System.out.println(player1.getPartsOfShipAsString(player1.getShip(i)));
+                    System.out.println(player1.getBoardAsString());
+                    valid = true;
+                } catch (NotAlignedShipException | NotInsideTheBoardException | InvalidShipLengthException |
+                         TooCloseToAnotherShipException | ShipOverlapException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    private void fire() {
+        System.out.println(Display.gameStart());
+        System.out.println(player1.getFogWarBoard());
+
+        {
+            boolean valid = false;
+            while(!valid) {
+                System.out.println(Display.takeAShot());
+                String fireShot = sc.nextLine();
+                try {
+                    boolean isFireOnShip = player1.fire(fireShot);
+                    System.out.println(player1.getFogWarBoard());
+                    System.out.println(Display.isFireOnShip(isFireOnShip));
+                    valid = true;
+                } catch (NotInsideTheBoardException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+}
--- src/battleship/app/Main.java
+++ src/battleship/app/Main.java
@@ -9,48 +9,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        Player player1 = new Player();
-        System.out.println(player1.getBoardAsString());
-
-        for(int i = 0; i < 5; i++) {
-            boolean valid = false;
-            while(!valid) {
-                System.out.println(Display.placeShip(i));
-                String shipPositions = sc.nextLine();
-                try {
-                    player1.createFleet(shipPositions, i);
-                    System.out.println(player1.getLengthOfShipAsString(player1.getShip(i)));
-                    System.out.println(player1.getPartsOfShipAsString(player1.getShip(i)));
-                    System.out.println(player1.getBoardAsString());
-                    valid = true;
-                } catch (NotAlignedShipException | NotInsideTheBoardException | InvalidShipLengthException |
-                         TooCloseToAnotherShipException | ShipOverlapException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        }
-
-        System.out.println(Display.gameStart());
-        System.out.println(player1.getFogWarBoard());
-
-        {
-            boolean valid = false;
-            while(!valid) {
-                System.out.println(Display.takeAShot());
-                String fireShot = sc.nextLine();
-                try {
-                    boolean isFireOnShip = player1.fire(fireShot);
-                    System.out.println(player1.getFogWarBoard());
-                    System.out.println(Display.isFireOnShip(isFireOnShip));
-                    valid = true;
-                } catch (NotInsideTheBoardException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        }
-
-        sc.close();
+        GameEngine gameEngine = new GameEngine();
+        gameEngine.startGame();
     }
 }
